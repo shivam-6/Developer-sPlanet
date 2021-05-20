899ad616dc2eb3f1650fa1dd60ccbf1a918387ce
shivam-6 <shivamvishvakarma6@gmail.com>
2021-05-20 11:42:53 +0530

    commit

--- src/app/query-details/query-details.component.ts
+++ src/app/query-details/query-details.component.ts
@@ -27,6 +27,6 @@ export class QueryDetailsComponent implements OnInit {
 
   addSolution() {
     sessionStorage.setItem('query_to_answer', JSON.stringify(this.queryData));
-    this.router.navigate(['/user/addsolution']);
+    this.router.navigate(['/addsolution']);
   }
 }
